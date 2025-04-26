eeb8b7ce4537a1316ab28f4fb9d815bbe6a78ccf
ToFik <tofikpetya@gmail.com>
2025-04-27 00:23:55 +0300

    hotfix (just added from)

--- src/main/java/org/piva/fisdtest26042025/util/EmailSenderUtil.java
+++ src/main/java/org/piva/fisdtest26042025/util/EmailSenderUtil.java
@@ -1,6 +1,7 @@
 package org.piva.fisdtest26042025.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,12 @@ public class EmailSenderUtil {
 
     private final JavaMailSender mailSender;
 
+    @Value("${MAIL_USERNAME}")
+    private String from;
+
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
